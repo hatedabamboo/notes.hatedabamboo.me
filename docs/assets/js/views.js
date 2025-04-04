c57d695b99bc4f23d1853910ab445de346130ea2
hatedabamboo <hatedabamboo@pm.me>
2025-04-04 09:02:18 +0200

    Add views counter on post page

--- docs/assets/js/views.js
+++ docs/assets/js/views.js
@@ -1,14 +1,21 @@
 async function updateViewCounter() {
     const pageUrl = encodeURIComponent(window.location.pathname);
+    const fallbackValue = "?";
     try {
         const response = await fetch(`https://api.hatedabamboo.me/views?pageUrl=${pageUrl}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        document.getElementById("counter").innerText = data.views || "❔";
+        const counters = document.querySelectorAll("#counter");
+        counters.forEach(counter => {
+            counter.innerText = data.views || fallbackValue;
+        });
     } catch (error) {
-        document.getElementById("counter").innerText = "❔";
+        const counters = document.querySelectorAll("#counter");
+        counters.forEach(counter => {
+            counter.innerText = fallbackValue;
+        });
         console.error("Error updating view counter:", error);
     }
 }
