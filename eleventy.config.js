bd4901dd70bade13813911f3673d401cbc44573d
hatedabamboo <hatedabamboo@pm.me>
2025-09-27 10:07:28 +0100

    Add "Speaking" section to the website

--- eleventy.config.js
+++ eleventy.config.js
@@ -50,6 +50,7 @@ export default async function (eleventyConfig) {
     },
   });
   eleventyConfig.addPlugin(EleventyPluginOgImage, {
+    previewMode: false,
     satoriOptions: {
       fonts: [
         {
